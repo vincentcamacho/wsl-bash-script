4518e455d5235bc7bfe782c88bb681c65339a4ca
jvinc86 <jvincent.camacho@gmail.com>
2022-04-08 21:48:22 +0200

    Update wsl-bash-script.sh

--- wsl-bash-script.sh
+++ wsl-bash-script.sh
@@ -2,6 +2,7 @@
 
 # Cambiar nombre de maquina wsl
 nombre_maquina=ubuntu-wsl
+sudo hostname -b $nombre_maquina
 sudo sed -i "s/$HOSTNAME/$nombre_maquina/g" /etc/hosts
 
 # Otra forma de cambiar nombre de la maquina pero NO funciona en WSL
